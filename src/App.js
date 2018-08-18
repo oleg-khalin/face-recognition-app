db030283b336a8429a4dfebffa97b886e3989bf9
Oleg Khalin <olegkhalin18@gmail.com>
2018-08-18 19:03:32 +0300

    Created all the components we need and finished all visualisation.

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 123,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: '#ffffff'
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000'
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: 'img/github.svg',
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY:1200
+      }
+    }
+  },
+  retina_detect: true
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className='particles'
+          params={ particlesOptions }
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*        <FaceRecognition />*/}
+      </div>
+    );
+  }
+}
+
+export default App;
