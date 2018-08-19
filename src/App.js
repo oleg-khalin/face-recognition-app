71f4d62b62e6ff3896cbaf8a7e712f7f6f2832ca
Oleg Khalin <olegkhalin18@gmail.com>
2018-08-19 19:48:02 +0300

    Now API is connected and gives us results of the face location

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -88,23 +89,23 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({ imageUrl: this.state.input });
     app.models
       .predict(
-      Clarifai.COLOR_MODEL,
-          // URL
-          "https://samples.clarifai.com/metro-north.jpg"
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input
       )
       .then(function(response) {
-          console.log(response);
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function(err) {
         // there was an error
@@ -126,7 +127,7 @@ class App extends Component {
           onInputChange={ this.onInputChange }
           onButtonSubmit={ this.onButtonSubmit }
         />
-        {/*        <FaceRecognition />*/}
+        <FaceRecognition imageUrl={ this.state.imageUrl } />
       </div>
     );
   }
